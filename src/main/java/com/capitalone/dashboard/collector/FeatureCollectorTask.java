ff03f1c25aa4550d4c7598b52ceb6e1da7179dca
maximiliano-b <51840591+maximiliano-b@users.noreply.github.com>
2022-04-18 09:00:57 -0700

    Update src/main/java/com/capitalone/dashboard/collector/FeatureCollectorTask.java
    Co-authored-by: sonatype-lift[bot] <37194012+sonatype-lift[bot]@users.noreply.github.com>

--- src/main/java/com/capitalone/dashboard/collector/FeatureCollectorTask.java
+++ src/main/java/com/capitalone/dashboard/collector/FeatureCollectorTask.java
@@ -362,7 +362,7 @@ public class FeatureCollectorTask extends CollectorTask<FeatureCollector> {
      /**
      * convert the utc timestamp
      *
-     * @param features
+     * @param feature
      * @param str
      */
     private void utcTimeStamp(String str, Feature feature) {
